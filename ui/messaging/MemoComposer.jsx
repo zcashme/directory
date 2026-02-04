389134a8df9b156c950626e829d7d2973779ee9a
Julian Abraham <julian.abraham@xaviers.edu.in>
2026-02-04 14:24:36 +0800

    refactor: move common hooks to ui/

--- ui/messaging/MemoComposer.jsx
+++ ui/messaging/MemoComposer.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo, useRef, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { QRCodeSVG } from "qrcode.react";
 import { useFeedback, useFeedbackController } from "@/lib/messaging/useFeedback";
-import useEmojiAutocomplete from "@/lib/useEmojiAutocomplete";
+import useEmojiAutocomplete from "@/ui/messaging/useEmojiAutocomplete";
 import AmountAndWallet from "@/ui/verification/AmountAndWallet";
 import HelpMessage from "@/ui/verification/HelpMessage";
 
