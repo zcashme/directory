2457021cc8feafb6ea63b5e857ac8450428830fd
james-eda <james@edaclinical.com>
2026-03-07 04:42:36 -0500

    fix(layout): hide floating sidebar menu behind feature toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ interface RootLayoutProps {
   children: ReactNode;
 }
 
+const SHOW_FLOATING_SIDEBAR_MENU = false;
+
 const SOCIAL_LINKS = [
   { href: "https://x.com/zcashme", label: "X (Twitter)", path: "M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z", evenodd: false },
   { href: "https://discord.gg/z2H23QgAGf", label: "Discord", path: "M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.29-.444.67-.608 1.06a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.06.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z", evenodd: false },
@@ -22,7 +24,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     <html lang="en">
       <body className="min-h-screen flex flex-col" style={{ backgroundColor: 'var(--color-background)' }}>
         <ProfileHeader profileCount={profileCount} />
-        <FloatingSidebarMenu />
+        {SHOW_FLOATING_SIDEBAR_MENU ? <FloatingSidebarMenu /> : null}
         <div className="flex-1">{children}</div>
         <footer className="relative border-t border-gray-200" style={{ backgroundColor: "var(--color-background)" }}>
           <div className="max-w-5xl mx-auto px-4">
